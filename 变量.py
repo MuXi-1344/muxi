c88e41984b2dc8995b760cbda93b4a28a378425b
muxi1344 <dulijuan6666@163.com>
2020-12-18 10:15:18 +0800

    更新变量

---
+++ 变量.py
@@ -0,0 +1,28 @@
+# 1、数字类型
+# int_a = 1
+# float_a = 0.2
+# complex_a = 0.2j
+#  通过type查看变量的数据类型
+# print(type(int_a))
+# print(type(float_a))
+# print(type(complex_a))
+
+# 2、字符串 string
+#str_b = "abcd\\n"
+#str_b = r"abcd\n"
+""" str_b = "abcd"
+str_c = "efg"
+print(str_b)
+print(str_c)
+print(type(str_b))
+print(str_b+str_c) """
+# 切片 【开始：结束：步长】
+sti_d = "1234567"
+print(sti_d[0:5:2])
+print(sti_d[0:5])
+print(sti_d)
+
+# 3、列表list
+list1 = [1,2,3,4,5]
+print(list1)
+print(list1[0:5:2])
